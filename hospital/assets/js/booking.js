24f1b2b22e893cbd968e064acd1ea188538fa94f
Ravi <ravim1032@aspl.ai>
2025-07-18 19:39:51 +0530

    added new update

--- hospital/assets/js/booking.js
+++ hospital/assets/js/booking.js
@@ -28,7 +28,7 @@ function handleClick(event) {
         name: document.getElementById('name').value.trim(),
         phone_number: document.getElementById('phone_number').value.trim(),
         location: document.getElementById('location').value.trim(),
-        treatment: document.getElementById('treatment').value.trim()
+    
     };
 
         const missingFields = [];
@@ -42,9 +42,7 @@ function handleClick(event) {
     if (!bookingData.location) {
         missingFields.push('location');
     }
-    if (!bookingData.treatment) {
-        missingFields.push('treatment');
-    }
+   
 
     if (missingFields.length > 0) {
         alert('Please fill in the required field(s): ' + missingFields.join(', '));
